e2bb8c4e96ec64370bbac3ce8fbd6c4ed8cf9050
Gabriel <business.gabrielgoncalves@gmail.com>
2023-09-29 18:51:40 -0300

    primeiro commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+dynamic tdRed = const Color(0xFFDA4040);
+dynamic tdBlue = const Color(0xFF5F52EE);
+dynamic tdBlack = const Color(0xFF3A3A3A);
+dynamic tdGrey = const Color(0xFF717171);
+dynamic tdBGColor = const Color(0xFFEEEFF5);
